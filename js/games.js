13108d12c025845de11d4ff485814aaba3fe873a
oseaersole90 <oseaersole90@gmail.com>
2025-02-06 16:24:57 +0700

    Update games.js

--- js/games.js
+++ js/games.js
@@ -1,7 +1,7 @@
 const games = [
     { name: "Vex 8", image: "image/vex-8.png", description: "Endless Runner", link: "/vex-8.html", category: "Arcade,Action" },
     { name: "Slope", image: "image/slope.png", description: "Endless Runner", link: "/slope.html", category: "Arcade,Action" },
-
+    { name: "a", image: "image/slope.png", description: "Endless Runner", link: "/run-3.html", category: "Arcade,Action" },
 ];
 
 /*createGameCard*/
