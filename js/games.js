3fdc561a40444383d8b99f3dff8e22e7cea39e0b
Linh Hoang <>
2025-02-10 14:18:09 +0700

    add games
    Linh

--- js/games.js
+++ js/games.js
@@ -2,6 +2,7 @@ const games = [
     { name: "Vex 8", image: "image/vex-8.png", description: "Endless Runner", link: "/vex-8.html", category: "Arcade,Action" },
     { name: "Slope", image: "image/slope.png", description: "Endless Runner", link: "/slope.html", category: "Arcade,Action" },
     { name: "run 3", image: "image/slope.png", description: "Action", link: "/run-3.html", category: "Action" },
+	{ name: "Gun Mayhem", image: "image/vex-8.png", description: "Action, Shooting", link: "/run-3.html", category: "Action,Shooting" },
 ];
 
 /*createGameCard*/
